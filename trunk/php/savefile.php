723c7e5be4e2f4bbba003a8a6ac57c0c14afb144
jeratt5 <jeratt5@d660baa4-9fee-11de-98a9-4d1d5fda2735>
2009-09-14 11:55:22 +0000

    made database store thumbnails

--- trunk/php/savefile.php
+++ trunk/php/savefile.php
@@ -1,6 +1,6 @@
 <?php
 	include_once "ez_sql.php";
 	
-	$db->query('INSERT INTO gallery (name, author, data) VALUES ("' . $_POST['name'] . '", "' . $_POST['author'] . '", "' . $_POST['data'] . '")');
+	$db->query('INSERT INTO gallery (name, author, data, thumb) VALUES ("' . $_POST['name'] . '", "' . $_POST['author'] . '", "' . $_POST['data'] . '", "' . $_POST['thumb'] . '")');
 	echo $db->insert_id;
 ?>
