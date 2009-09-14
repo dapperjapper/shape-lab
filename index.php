7aec2f648e93c2f6eebe641e5cc5b91e31ed7a41
lclarkberg <lclarkberg@d660baa4-9fee-11de-98a9-4d1d5fda2735>
2009-09-14 00:21:06 +0000

    added test index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Shape Lab</title>
+<link type="text/css" href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+	<div id="page_centering">
+		<p>test</p>
+	</div>
+</body>
+</html>
